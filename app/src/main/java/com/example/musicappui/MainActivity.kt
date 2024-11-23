dd3ed0506f2ca21a193ea585aa10644786e43f05
vpennacchia <penna.vitto@gmail.com>
2024-11-23 13:51:44 +0100

    .

---
+++ app/src/main/java/com/example/musicappui/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.musicappui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.musicappui.ui.theme.MainView
+import com.example.musicappui.ui.theme.MusicAppUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MusicAppUITheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainView()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MusicAppUITheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/musicappui/ui/theme/BrowseView.kt
@@ -0,0 +1,18 @@
+package com.example.musicappui.ui.theme
+
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import com.example.musicappui.R
+
+@Composable
+fun Browse() {
+    val categories = listOf("Hits", "Happy", "Workout", "Running", "TGIF", "Yoga")
+    LazyVerticalGrid(GridCells.Fixed(2)) {
+        items(categories) { cat ->
+            BrowserItem(cat = cat, drawable = R.drawable.baseline_apps_24)
+
+        }
+    }
+}
